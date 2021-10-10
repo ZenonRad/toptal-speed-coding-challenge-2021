bda9e51a7fe439f1923d85cac128608a637b35fd
ZenonRad <herizo@hairun-technology.com>
2021-10-10 23:07:37 +0300

    🐛 Fix digitOccurrence algorithm

--- challenge/dist/solutions/digitOccurrence.js
+++ challenge/dist/solutions/digitOccurrence.js
@@ -1,8 +1,9 @@
 box.digitOccurrence = function digitOccurrence(n, x) {
   let count = 0;
+  const len = n.toString().length;
 
-  for (let di = 0; di < n.toString().length; di++) {
-    const power = parseInt(Math.pow(10, di));
+  for (let i = 0; i < len; i++) {
+    const power = parseInt(Math.pow(10, i));
     const down = n - (n % (power * 10));
     const up = down + power * 10;
     const digit = parseInt(n / power) % 10;
@@ -10,6 +11,12 @@ box.digitOccurrence = function digitOccurrence(n, x) {
     if (digit < x) count += down / 10;
     else if (digit > x) count += up / 10;
     else count += down / 10 + (n % power) + 1;
+  }
+
+  if (x === 0) {
+    let diff = 0;
+    for (let i = 1; i < len; i++) diff += Math.pow(10, i);
+    count -= diff;
   }
 
   return count;
